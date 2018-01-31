cae1347a39124aef20c6fa84d1c4f3a863dba183
ezgioral <35849888+ezgioral@users.noreply.github.com>
2018-01-31 14:15:12 +0300

    Update Nakagami_deneme1.m

--- Nakagami_deneme1.m
+++ Nakagami_deneme1.m
@@ -7,21 +7,21 @@ clear; clc; close all;dbstop if error;
 p = 1; %Signal power 
 MaxIt = 2000;
 SNR = [0:1:20];  
-epsilon = 1/100;  %step size determined random
+epsilon = 1/100;  
 f1 = figure;
 f2 = figure;
 min_node = 3;
-max_node = 4;
+max_node = 8;
 
 %% This is for naming the data
 number = [min_node:1:max_node]';
 names = int2str(number);
                                 
-Allowed_Error = 1/100; 
+Allowed_Error = 1/10000; 
 for nodes = min_node:1:max_node
     
 N = nodes^2 ;
-Edge_num = nchoosek(nodes,2);%for us to be able to define any number of edges
+Edge_num = nchoosek(nodes,2);
 monte_counter = 0;
 counter = 0;
 switch nodes
@@ -44,11 +44,11 @@ sz = size(Measured);
 Mysum = sum(Measured);
 Average = Mysum/sz(1);
 %% Starting to Monte Carlo
-montemax = 1e3;                           %how many times monte carlo                         
-L_Networks = zeros(montemax,nodes,nodes); %thil is L matrix container
-Ranks = zeros(montemax,6);                %this will keep everything about matrices
+montemax = 1e5;                                                
+L_Networks = zeros(montemax,nodes,nodes); 
+Ranks = zeros(montemax,6);              
 for SNR_counter = 1:length(SNR) 
-    tic %alttaki yazı 20 defa tekrarlanacak.Word'e kopyala ve onları say. Kalan zamanı hesapla.
+    tic 
 for monte = 1:montemax
     %Nakagami başlangıcı
     R = 1 ; 
@@ -66,39 +66,39 @@ if Naka_m==0.5    %%Gauss
 else
         h_Naka=sqrt( sum(h));
 end
-%%%%mean(h_Naka.^2);  kullanmak gerekir mi?
+
 gama = 10^(SNR(SNR_counter)/10); 
 threshold = (2^R - 1) / gama ;
  Edge_con = abs(h_Naka).^2 > threshold ;
 
-StateContainer = zeros(size(Measured,1), MaxIt + 1 ); %for every monte, this will keep states
+StateContainer = zeros(size(Measured,1), MaxIt + 1 ); 
      
-     StateContainer(:,1) = Measured(:); %içine alamıyor tranpoz sorunu
+     StateContainer(:,1) = Measured(:); 
      
-     FeedbackContainer = zeros(MaxIt,size(Measured,2));   %for every monte this will keep feedbacks
+     FeedbackContainer = zeros(MaxIt,size(Measured,2));  
 
 
       
      
-     [ii,jj] = ndgrid(1:nodes);              % Used to choose the upper triangle of The Matrix
-      A = zeros(nodes);                       % Adjacency Matrix's for all
-      A(jj>ii) =  Edge_con;                   % Filling the upper Triangle
+     [ii,jj] = ndgrid(1:nodes);            
+      A = zeros(nodes);                       
+      A(jj>ii) =  Edge_con;                   
      
       
-      A = A + A';                             % Adjacency Matrix created
-      D = diag(sum(A));                       % In-Degree Matrix created  
-      L = D - A ;                             % Laplacian Matrix
+      A = A + A';                             
+      D = diag(sum(A));                        
+      L = D - A ;                             
            
-      L_Networks(monte,:,:) = L ; %her bir monte değişkeni birer L A ve D oluşturacak
+      L_Networks(monte,:,:) = L ; 
 
      
-      Ranks(monte,1) = rank(L) ;  %her bir monte değişkeni için matrix rank toplamaca
+      Ranks(monte,1) = rank(L) ;  
       
-      %% Now let's start iterating for every matrix in monte carlo
+      %%  monte carlo
       
-      if rank(L) == nodes - 1 %bu sağlanmıyorsa alphaya ulaşılamadı ranks5 = 0
+      if rank(L) == nodes - 1 
      
-      Ranks(monte,4) = 1; %we can reach alpha,consensus
+      Ranks(monte,4) = 1; 
       monte_counter = monte_counter + 1;
       
       for k = 1:MaxIt
@@ -115,12 +115,10 @@ StateContainer = zeros(size(Measured,1), MaxIt + 1 ); %for every monte, this wil
           counter = 0;
           break 
       end
-      
-      end
-      
-      else
+            end
+            else
       Ranks(monte,4) = 0;
-      Ranks(monte,5) = 0; %bunun bu şekilde olmaması gerekiyor. iterasyonu önle
+      Ranks(monte,5) = 0; 
       end
       
 end
